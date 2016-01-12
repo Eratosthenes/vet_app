56e32c26aa4d04ebf37f96017d683101bbfe5edf
Nathan Rapport <n.rapport@gmail.com>
2016-01-12 12:18:31 -0800

    first commit

---
+++ app/models/order.rb
@@ -0,0 +1,28 @@
+class Order < ActiveRecord::Base
+  attr_writer :current_step
+
+  def steps
+    %w[shipping billing confirmation]
+  end
+
+  def current_step
+    @current_step || steps.first
+  end
+
+  def next_step
+    self.current_step = steps[steps.index(current_step)+1]
+  end
+
+  def previous_step
+    self.current_step = steps[steps.index(current_step)-1]
+  end
+
+  def first_step?
+    current_step == steps.first
+  end
+
+  def last_step?
+    current_step == steps.last
+  end
+
+end
